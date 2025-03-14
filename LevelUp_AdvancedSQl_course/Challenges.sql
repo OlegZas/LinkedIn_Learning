8298b1e87d1fc17eaee07fcc058ee85bc841e5bf
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-03-13 21:29:35 -0700

    Update Challenges.sql

--- LevelUp_AdvancedSQl_course/Challenges.sql
+++ LevelUp_AdvancedSQl_course/Challenges.sql
@@ -2,3 +2,9 @@
 SELECT e.employeeId, e.firstName, e.lastName, e2.employeeId, e2.firstName || ' '|| e2.lastName as Manager
 FROM employee e
 INNER JOIN employee e2 ON e.employeeId = e2.managerId
+
+-- 2. Get a list of salespeople with zero sales
+SELECT e.firstName, e.lastName, s.salesAmount 
+FROM employee e
+LEFT JOIN sales s ON e.employeeId = s.employeeId 
+WHERE s.employeeId IS NULL ;
